dc0bbafbf6ce75b7958916afcdea89cd0b6ea7de
modest2798 <lungociumodestconstantin@gmail.com>
2018-10-13 16:48:06 +0100

    check new version

--- app.py
+++ app.py
@@ -1,8 +1,23 @@
-import spotipy
 import requests
+import spotipy
 
 spotify = spotipy.Spotify()
+
 req = requests.get(server, auth=('user',"pass"))
 print(req.text)
+
+
+if len(req.text) > 1:
+    name = ' '.join(req.text[1:])
+else:
+    name = 'Sample'
+
+results = spotify.search(q='artist:' + name, type='artist')
+items = results['artists']['items']
+if len(items) > 0:
+    artist = items[0]
+    print artist['name'], artist['images'][0]['url']
+
+
 
 
