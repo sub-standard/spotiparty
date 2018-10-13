19ea6a0d378f0ca0786948f752a3bf8c119b1301
aledjackson <aledjackson24@gmail.com>
2018-10-13 23:42:39 +0100

    added skip song functionality

--- app.py
+++ app.py
@@ -1,48 +1,49 @@
 import spotipy
 import nltk as nl
-# import Levenshtein_search as lev
-token = "BQBvNJRBoNSPlHBuDDmHbY6joAFt7IhcrJKgaCcfR1MSPNrQgHz2jAkiib5-PRwueWIey8RvaKJv5nmjgI003f-7Wb_W7Y27SPaWqP7utdVM8g5fCaDVVcB71N3iHr0oN9_tlyW669hVvU8fVngu7NQ3UU8dFbIS3O9K_WpBOihb3zSu7kM"
+import Levenshtein_search as lev
+import difflib
+# token = "BQBlaWxoykhL6HQpm_D1w0yuL62sZvRi_cw5vXH8lTer45zv-ORJ9VeW45AExpUcsEBg6y661VqS6HbdX36S1XBJp9RQV6j6Nh2SCcDYeHNpRcGNSSv1sRkew5LYmaK28Gf4wTDNh5_5Iejux_IGg1LE637rFsd4TYDfpvLXvbD5PVHtM979c2Ts"
 
 def get_track_id(query,token):
+    print(query)
+    if(query == ""):
+        return None
     spotify = spotipy.Spotify(auth=token)
     results = spotify.search(query, limit=1)
+    if(len(results['tracks']['items']) == 0):
+        return get_track_id(query[:-1],token)
+    print(results['tracks']['items'][0]['name'])
     return results['tracks']['items'][0]['id']
-# def get_track_name(query,token):
-#     spotify = spotipy.Spotify(auth=token)
-#     results1 = spotify.search(name, limit = 100)
-#     the_song = ""
-#     max = 0
-#     for i in range(0, len(results1)):
-#         word = results1(i)
-#         max1 = lev.ratio(query, word)
-#         if(max1>max):
-#             max = max1
-#             the_song = word
-#
-#     return the_song
-#
-# get_track_name("bump'n'grind",token)
-# def track_on_lyrics(query,token):
-#
-#     eLyrics = []
-#
-#     for item in gdata:
-#         title = item.find_all('a', {'itemprop': 'name'})[0].text
-#         lyricsdotcom = 'http://www.lyrics.com'
-#         for link in item('a'):
-#             try:
-#                 lyriclink = lyricsdotcom + link.get('href')
-#                 req = requests.get(lyriclink)
-#                 lyricsoup = BeautifulSoup(req.content)
-#                 lyricdata = lyricsoup.find_all('div', {'id': re.compile('lyric_space|lyrics')})[0].text
-#                 eLyrics.append([title, lyricdata])
-#             except:
-#                 pass
+
+
+def get_track_name(query, token):
+    if (query == ""):
+        return None
+    spotify = spotipy.Spotify(auth=token)
+    results1 = spotify.search(query, limit=50)
+    # print (results1)
+    the_song = ""
+    max = 0
+    if len(results1['tracks']['items']) == 0:
+        return get_track_name(query[:-1], token)
+    for item in results1['tracks']['items']:
+        # print (item['name'])
+        word = item['name']
+        max1 = difflib.SequenceMatcher(None, word, query).ratio()
+        # print(max1)
+        if (max1 >= max):
+            max = max1
+            the_song = word
+
+    return the_song
 
 
 
 
 
+
+
+# print(get_track_name("super massive blak hole", token))
 
 
 
