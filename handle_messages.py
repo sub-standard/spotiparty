600fc93d75e67f884a1015ea000e24b771940b58
aledjackson <aledjackson24@gmail.com>
2018-10-13 15:35:57 +0100

    handle_add_user() half completed

--- handle_messages.py
+++ handle_messages.py
@@ -1,9 +1,15 @@
+from state import state
 
-def handle_add_user():
-  pass
+def handle_add_user(sender, room_number):
+    rooms = state['rooms']
+    if room_number in rooms:
+        room = rooms[room_number]
+        room['phone_number'].append(sender)
+        send_text(sender, "added to room " + room_number)
+
 
 def handle_add_song():
-
+    pass
 
 
 
