b419391ae8dbd095f0d4ece54cd6f5134c4f153e
aledjackson <aledjackson24@gmail.com>
2018-10-13 15:02:25 +0100

    fixed delivery_reciept

--- server.py
+++ server.py
@@ -1,7 +1,7 @@
 from flask import Flask, request, jsonify
 from pprint import pprint
-from nexmo import handle_join, handle_add_song
-
+from handle_messages import handle_add_user, handle_add_song, send_text
+import re
 app = Flask(__name__)
 
 @app.route('/create-room', methods=['GET', 'POST'])
@@ -14,11 +14,25 @@ def create_room():
 
 @app.route('/nexmo', methods=['GET', 'POST'])
 def delivery_receipt():
-    if 'add_user':
-        handle_add_user()
+    print("recieved a request")
+    if request.is_json:
+        pprint(request.get_json())
+        text_message = request.get_json['text']
+        sender = request.get_json['msisdn']
+        if "room" in text_message:
+            room_number = re.search(r"join (\d+)",text_message)
+            if room_number == None:
+                send_text("incorect command", sender)
+            else:
+                handle_add_user(sender, room_number)
+        elif "add" in text_message:
+            song_query = re.search(r"add (.+)" , text_message)
+            handle_add_song(song_query)
 
-    if 'add_song':
-        handle_add_song()
+    else:
+        data = dict(request.form) or dict(request.args)
+        pprint(data)
+
 
     return None
 
