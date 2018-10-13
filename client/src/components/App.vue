1b77f07b829742f87d492aa95ffb2713acf5f43f
Jordan Mussi <JordanMussi@users.noreply.github.com>
2018-10-13 15:22:01 +0100

    Register MakeRoom component

--- client/src/components/App.vue
+++ client/src/components/App.vue
@@ -5,11 +5,14 @@
 </template>
 
 <script>
-  import MakeRoom from './MakeRoom'
+import MakeRoom from './MakeRoom'
 
-  export default {
-    name: 'app'
+export default {
+  name: 'app',
+  components: {
+    MakeRoom
   }
+}
 </script>
 
 <!-- CSS libraries -->
@@ -17,53 +20,59 @@
 
 <!-- Global CSS -->
 <style>
-  code {
-    font-family: Menlo, Monaco, Lucida Console, Liberation Mono, DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace, serif;
-    font-size: 0.9em;
-    white-space: pre-wrap;
-    color: #2c3e50;
-  }
+code {
+  font-family: Menlo, Monaco, Lucida Console, Liberation Mono, DejaVu Sans Mono,
+    Bitstream Vera Sans Mono, Courier New, monospace, serif;
+  font-size: 0.9em;
+  white-space: pre-wrap;
+  color: #2c3e50;
+}
 
-  code::before, code::after {
-    content: '`';
-  }
+code::before,
+code::after {
+  content: '`';
+}
 </style>
 
 <!-- Scoped component css -->
 <!-- It only affect current component -->
 <style scoped>
-  #app {
-    text-align: center;
-  }
+#app {
+  text-align: center;
+}
 
-  #app h1 {
-    color: #2c3e50;
-    font-weight: 300;
-    margin: 0;
-  }
+#app h1 {
+  color: #2c3e50;
+  font-weight: 300;
+  margin: 0;
+}
 
-  .banner {
-    height: 200px;
-    background-color: #f6f6f6;
-    padding: 50px 10px;
-  }
+.banner {
+  height: 200px;
+  background-color: #f6f6f6;
+  padding: 50px 10px;
+}
 
-  .bottom {
-    padding: 80px 10px;
-    font-size: 24px;
-    font-weight: 300;
-  }
+.bottom {
+  padding: 80px 10px;
+  font-size: 24px;
+  font-weight: 300;
+}
 
-  .fade {
-    font-size: 14px;
-  }
+.fade {
+  font-size: 14px;
+}
 
-  .logo {
-    animation: spin 4s 1s infinite linear
-  }
+.logo {
+  animation: spin 4s 1s infinite linear;
+}
 
-  @keyframes spin {
-    from {transform:rotate(0deg);}
-    to {transform:rotate(360deg);}
+@keyframes spin {
+  from {
+    transform: rotate(0deg);
   }
+  to {
+    transform: rotate(360deg);
+  }
+}
 </style>
