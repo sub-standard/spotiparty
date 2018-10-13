445a456842061a35ec455f705ed7545a894e46d9
Jordan Mussi <JordanMussi@users.noreply.github.com>
2018-10-13 14:59:51 +0100

    Add MakeRoom component

--- client/src/components/App.vue
+++ client/src/components/App.vue
@@ -1,24 +1,12 @@
 <template>
   <div id="app">
-    <div class="banner">
-      <img
-        src="https://vuejs.org/images/logo.png"
-        width="100"
-        alt="vue"
-        class="logo"
-      />
-      <h1>Welcome to Vue.js</h1>
-    </div>
-    <div class="bottom">
-      To get started, edit <code>./src/components/App.vue</code> and save to reload.<br/>
-      <span class="fade">
-        Checkout <code>./README.md</code> for more usages.
-      </span>
-    </div>
+    <MakeRoom />
   </div>
 </template>
 
 <script>
+  import MakeRoom from './MakeRoom'
+
   export default {
     name: 'app'
   }
---
+++ client/src/components/MakeRoom.vue
@@ -0,0 +1,11 @@
+<template>
+  <div id="make-room">
+    <h1>Make a new Room</h1>
+  </div>
+</template>
+
+<script>
+  export default {
+
+  }
+</script>
